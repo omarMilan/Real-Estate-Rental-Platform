a47967743d24c5696ad65acca90c9a068f3d3111
omarMilan <131297774+omarMilan@users.noreply.github.com>
2025-01-13 21:53:40 -0600

    test

--- projectFolder/src/components/contactPageComponents/contactAgentInfo.jsx
+++ projectFolder/src/components/contactPageComponents/contactAgentInfo.jsx
@@ -3,20 +3,20 @@ import agentPic from "../../assets/otherImages/agentHeadShot.webp";
 export default function ContactAgentInfo({ className }) {
   return (
     <div
-      className={`w-[610px] rounded-sm h-[382px] flex flex-col justify-center items-center absolute bg-slate-400 ${className}`}
+      className={`w-[610px] rounded-sm h-[382px] flex flex-col justify-center items-center absolute ring-2 ring-slate-400 border-2 border-black hover:border-slate-400 hover:ring-black transition-all duration-500 bg-slate-400 ${className}`}
     >
       {/* Agent Picture */}
       <img
         src={agentPic}
         alt="Agent"
-        className="h-48  flex top-0 absolute mt-8 rounded-sm shadow-sm ring-1 ring-slate-500 drop-shadow-sm"
+        className="h-48  flex top-0 absolute mt-8 rounded-sm shadow-sm ring-2 ring-slate-400 border-2 border-black hover:border-slate-400 hover:ring-black transition-all duration-500 drop-shadow-sm"
       />
 
       {/* Agent Information */}
       <div className="mt-48 text-center">
         {/* Name */}
         <p className="text-xl font-bold text-black">John Doe</p>
-        {/* Title */}
+        {/* Title  */}
         <p className="text-base text-slate-600">Realtor</p>
         {/* Email */}
         <p className="text-lg text-black">
